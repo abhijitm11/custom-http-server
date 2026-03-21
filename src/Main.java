43dc054749d069ee258a6234112546edd5688988
abhijit_mudoi <abhijitmudoi@gmail.com>
2026-03-21 22:24:14 +0530

    Added basic ServerSocket configuration to accept connection request

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import model.User;
+import server.HttpServer;
 
 public class Main {
     public static void main(String[] args) {
         User user = new User("1", "alex", "alex@test.com");
         System.out.println(user.toString());
+
+        HttpServer server = new HttpServer();
+        server.start(8080);
     }
 }
--- src/server/HttpServer.java
+++ src/server/HttpServer.java
@@ -1,4 +1,23 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpServer {
+    public void start(int port) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);   // Opens a port, listens for incoming connections
+            System.out.println("Server started on port: "+port);
+
+            while(true) {
+                Socket socket = serverSocket.accept();  // Waits until a client connects
+                System.out.println("Client Connected!");
+
+                // we'll handle request here
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
