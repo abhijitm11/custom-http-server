c10f096e9d9c40cdf83a0a365cac4ae16c2475f1
abhijit_mudoi <abhijitmudoi@gmail.com>
2026-03-21 23:07:50 +0530

    basic request handling and send back a response

--- src/server/HttpServer.java
+++ src/server/HttpServer.java
@@ -1,6 +1,9 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -8,16 +11,56 @@ public class HttpServer {
     public void start(int port) {
         try {
             ServerSocket serverSocket = new ServerSocket(port);   // Opens a port, listens for incoming connections
-            System.out.println("Server started on port: "+port);
+            System.out.println("Server started on port: " + port);
 
-            while(true) {
+            while (true) {
                 Socket socket = serverSocket.accept();  // Waits until a client connects
                 System.out.println("Client Connected!");
 
                 // we'll handle request here
+                handleRequest(socket);
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void handleRequest(Socket socket) {
+        try (
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                OutputStream out = socket.getOutputStream()
+        ) {
+            // read the request line
+            String requestLine = in.readLine();
+            System.out.println("Request: " + requestLine);
+
+            // read remaining header
+            String line;
+            while (!(line = in.readLine()).isEmpty()) {
+                // System.out.println(line);
+            }
+
+            // prepare response body
+            String responseBody = "Hello World from Server!";
+
+            // build proper http response
+            String response =
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: text/plain\r\n" +
+                            "Content-Length: " + responseBody.length() + "\r\n" +
+                            "\r\n" +
+                            responseBody;
+
+
+            // send response
+            out.write(response.getBytes());
+            out.flush();
+
+            // close connection
+            socket.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
